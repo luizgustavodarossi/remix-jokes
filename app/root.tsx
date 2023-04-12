052086d7ec7da1abd60fb0b69f87159965250db2
Luiz Gustavo Retka Darossi <luizgustavodarossi@gmail.com>
2023-04-12 20:44:44 -0300

    Explore the project structure

--- app/root.tsx
+++ app/root.tsx
@@ -1,25 +1,14 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { LiveReload } from "@remix-run/react";
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
+        <title>Remix: So great, it's funny!</title>
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
+        Hello world
         <LiveReload />
       </body>
     </html>
